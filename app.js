7373d480e4b1b7a886ad0afd5250b0276a7ecd5a
Bryan Thompson <bryan.thompson1223@gmail.com>
2023-07-31 09:02:08 -0500

    added gameBoard, infoDisplay, and a function to create the game board

---
+++ app.js
@@ -0,0 +1,11 @@
+const gameBoard = document.querySelector("#gameboard");
+const infoDisplay = document.querySelector("#info");
+
+const startCells = ["", "", "", "", "", "", "", "", ""];
+
+function createBoard() {
+  startCells.forEach((cell, index) => {
+    const cellElement = document.createElement("div");
+    cellElement.classList.add("square");
+  });
+}
